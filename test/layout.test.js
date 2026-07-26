4729d0855b646a417a589e828919e8d5508a8104
Evan Piercy <evanpiercy@gmail.com>
2026-07-26 09:06:57 -0700

    Add shelf layout tests

---
+++ test/layout.test.js
@@ -0,0 +1,16 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+
+import { chunkBooks } from "../layout.js";
+
+test("returns no chunks for an empty book list", () => {
+  assert.deepEqual(chunkBooks([], 5), []);
+});
+
+test("keep books together when they fit on one shelf", () => {
+  assert.deepEqual(chunkBooks([1, 2, 3, 4, 5], 5), [[1, 2, 3, 4, 5]]);
+});
+
+test("splits overflow into continued shelves in order", () => {
+  assert.deepEqual(chunkBooks([1, 2, 3, 4, 5, 6], 5), [[1, 2, 3, 4, 5], [6]]);
+});
